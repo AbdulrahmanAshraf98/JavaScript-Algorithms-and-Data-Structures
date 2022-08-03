9c42729cd107dd7a32ccd040c6f245f42f303d28
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-08-03 08:53:59 +0200

    extractMax From MaxBinary Heap

--- 2-DataStructure/5-heaps/MaxBinaryHeap/app.js
+++ 2-DataStructure/5-heaps/MaxBinaryHeap/app.js
@@ -20,14 +20,65 @@ class MaxBinaryHeap {
 		let childIndex = size - 1;
 		this.bubbleUp(childIndex);
 	}
+	sinkDown() {
+		let index = 0;
+		const length = this.values.length;
+		let element = this.values[0];
+		let leftChildIndex, rightChildIndex;
+		if (length === 1) return;
+		if (length === 2) {
+			leftChildIndex = 0;
+			rightChildIndex = 1;
+			if (this.values[rightChildIndex] > this.values[leftChildIndex]) {
+				this.values[leftChildIndex] = this.values[rightChildIndex];
+				this.values[rightChildIndex] = element;
+			}
+		}
+		if (length > 2) {
+			while (true) {
+				leftChildIndex = index * 2 + 1;
+				rightChildIndex = index * 2 + 2;
+				let leftChild, rightChild;
+				let swap = null;
+				if (leftChildIndex < length) {
+					leftChild = this.values[leftChildIndex];
+					console.log(leftChildIndex);
+					if (leftChild > element) swap = rightChildIndex;
+				}
+				if (rightChildIndex < length) {
+					rightChild = this.values[rightChildIndex];
+					if (
+						(rightChild > element && swap === null) ||
+						(swap !== null && rightChild > leftChild)
+					) {
+						console.log(rightChild);
+						swap = rightChildIndex;
+					}
+				}
+				if (swap === null) break;
+				this.values[index] = this.values[swap];
+				this.values[swap] = element;
+				index = swap;
+			}
+		}
+	}
+	extractMax() {
+		let max = this.values[0];
+		const end = this.values.pop();
+		if (!this.length) return;
+		this.values[0] = end;
+		this.sinkDown();
+		// console.log(this.values[0]);
+		return max;
+	}
 }
 
 const maxBinaryHeap = new MaxBinaryHeap();
 maxBinaryHeap.insert(41);
 maxBinaryHeap.insert(39);
 maxBinaryHeap.insert(33);
-maxBinaryHeap.insert(18);
-maxBinaryHeap.insert(27);
-maxBinaryHeap.insert(12);
-maxBinaryHeap.insert(55);
+console.log(maxBinaryHeap);
+maxBinaryHeap.extractMax();
+maxBinaryHeap.extractMax();
+maxBinaryHeap.extractMax();
 console.log(maxBinaryHeap);
